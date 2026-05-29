69e7376e51e6b1ed5c612a8c9d8706b8bb2f31db
baturfamily <goker.batur@gmail.com>
2026-05-29 14:12:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 
 const CONFIG = {
     // Google Apps Script deployment URL'si
-    API_URL: "https://script.google.com/macros/s/AKfycbzgyjpQB_iOVF0I4HYALWZ3cXB8whESU7z6-jipPqGvSmvS3xzhhDRMLrAkb69tKdo/exec",
+    API_URL: "https://script.google.com/macros/s/AKfycbyjR2xKYZi-lUMawklu4TMgnO2RO4ZJpQ67ibT08is9mIMLsnw8CkThutuLnFUgu77D4w/exec",
     
     // Uygulama başlığı (tarayıcı sekmesi ve header)
     APP_TITLE: "Finans Merkezi",
